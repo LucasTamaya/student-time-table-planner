7758810e1d9df914990193d34c52a29015883025
LucasTamayaGIT <lucastamaya.974@gmail.com>
2022-02-13 22:39:09 +0400

    Récupération des classes selectionnner dans la page my-classes

--- pages/api/class/[id].js
+++ pages/api/class/[id].js
@@ -1,5 +1,7 @@
 import { connectToDatabase } from "../../../util/mongodb";
 import NextCors from "nextjs-cors";
+// import { ObjectId } from "mongodb";
+const mongodb = require("mongodb");
 const jwt = require("jsonwebtoken");
 
 export default async function handler(req, res) {
@@ -11,14 +13,41 @@ export default async function handler(req, res) {
   );
 
   if (authenticatedStudent) {
-    const data = await db
+    //   classes sélectionnées par l'etudiant
+    const dataOne = await db
       .collection("students")
       .find({ rollNo: authenticatedStudent })
       .toArray();
-    if (data) {
-      const classes = data[0].classes;
-      console.log("réussi !", classes);
-      return res.status(200).json(classes);
+
+    //   toutes les classes
+    const dataTwo = await db.collection("classes").find({}).toArray();
+
+    if (dataOne && dataTwo) {
+      // conversion de la data brute
+      const selectedClassesId = dataOne[0].classes.map(
+        (x) => new mongodb.ObjectId(x)
+      );
+
+      //   tableau qui va contenir l'ensemble des propriétés des classes sélectionnées
+      const result = [];
+
+      //   compare les classes id selectionner avec l'ensemble des classes
+      selectedClassesId.map((x) => {
+        dataTwo.map((y) => {
+          // si l'id est identique, on ajoute la classe au tableau
+          if (JSON.stringify(y._id) === JSON.stringify(x)) {
+            result.push(y);
+            // console.log("valeur egale !!!!");
+            // sinon on ne fait rien
+          } else {
+            // console.log("valeur: ", typeof y._id, "filtre: ", typeof x);
+            // console.log("valeur differente");
+          }
+        });
+      });
+      console.log(result);
+      //   on renvoit le tableau au front
+      return res.status(200).json(result);
     } else {
       console.log("erreur in the fetch");
       return res.status(500);
@@ -30,3 +59,12 @@ export default async function handler(req, res) {
     return res.status(500);
   }
 }
+/*
+var reduced = options.reduce(function(filtered, option) {
+  if (option.assigned) {
+     var someNewValue = { name: option.name, newProperty: 'Foo' }
+     filtered.push(someNewValue);
+  }
+  return filtered;
+}, []);
+*/
--- pages/my-classes.js
+++ pages/my-classes.js
@@ -2,14 +2,22 @@ const axios = require("axios");
 import { useState, useEffect } from "react";
 
 export default function MyClasses() {
-  const [classes, setClassses] = useState();
+  const [myClasses, setMyClasses] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const deleteClass = async (id) => {
+
+  }; 
 
   useEffect(() => {
     fetch(
       `http://localhost:3000/api/class/${localStorage.getItem("accessToken")}`
     )
       .then((res) => res.json())
-      .then((data) => setClassses(data))
+      .then((data) => {
+        setMyClasses(data);
+        setLoading(false);
+      })
       .catch((err) => console.log(err));
   }, []);
 
@@ -17,17 +25,23 @@ export default function MyClasses() {
     <>
       <div>
         <h1 className="text-2xl font-bold">My Classes</h1>
-        {classes && (
-          <div>
-            {classes.map((x) => (
-              <div key={x}>
-                <p>{x}</p>
-              </div>
-            ))}
-            <h2>Number of classes per week:</h2>
-            <p>{classes.length}</p>
-          </div>
-        )}
+
+        {loading && <h1>Loading data ...</h1>}
+
+        {myClasses &&
+          myClasses.map((classe) => (
+            <div
+              key={classe._id}
+              className="border border-black w-60 mx-auto my-10 p-4 flex flex-col items-center rounded"
+            >
+              <h2>{classe.faculty}</h2>
+              <p>{classe.day}</p>
+              <p>
+                From {classe.from} to {classe.to}
+              </p>
+              <button className="bg-red-500 text-white px-3 py-1 rounded-xl cursor-pointer" onClick={() => deleteClass(classe._id)}>Delete</button>
+            </div>
+          ))}
       </div>
     </>
   );
